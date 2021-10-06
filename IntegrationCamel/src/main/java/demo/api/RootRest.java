861602398caf56bdd1c9622e2806f2ffcbe220bf
vargapa <pavol.varga@drei.com>
2021-10-06 11:24:08 +0200

    EAI Logging (Log4j2)

--- IntegrationCamel/src/main/java/demo/api/RootRest.java
+++ IntegrationCamel/src/main/java/demo/api/RootRest.java
@@ -1,5 +1,9 @@
 package demo.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping( "/api" )
 public class RootRest {
+  private static final Logger LOGGER = LogManager.getLogger( RootRest.class );
+  
+  
+  @GetMapping
+  public ResponseEntity<String> home() {
+    LOGGER.info( "home" );
+    return ResponseEntity.ok( "home" );
+  }
 }
--- IntegrationCamel/src/main/java/demo/service/IntegrationService.java
+++ IntegrationCamel/src/main/java/demo/service/IntegrationService.java
@@ -2,8 +2,8 @@ package demo.service;
 
 import demo.eai.FileTransfer;
 import org.apache.camel.CamelContext;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +13,15 @@ import javax.annotation.PreDestroy;
 @Service
 @Transactional
 public class IntegrationService {
+  private static final Logger LOGGER = LogManager.getLogger( IntegrationService.class );
+  
   FileTransfer fileTransfer;
   CamelContext context;
   
   
   public IntegrationService( FileTransfer fileTransfer, CamelContext context ) {
+    LOGGER.info( "CamelContext: {}", context.toString() );
+    
     this.fileTransfer = fileTransfer;
     this.context = context;
   }
